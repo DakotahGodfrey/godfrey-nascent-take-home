43206acd6910fd90b84b189a66e5847377325aab
Dakotah Godfrey <dakotah.godfrey.dev>
2022-07-16 12:33:32 -0400

    Configure Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
-
+import "styles/reset.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "pages/User";
+import Pokedex from "pages/Pokedex";
+import Confirm from "pages/Confirm";
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<User />}></Route>
+          <Route path={"/pokedex"} element={<Pokedex />}></Route>
+          <Route path={"/confirm"} element={<Confirm />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
