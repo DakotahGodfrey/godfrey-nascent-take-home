9611adfa9711b03a131624f3998dc11d1e58527d
Dakotah Godfrey <dakotah.godfrey.dev>
2022-07-19 13:16:30 -0400

    confirmation skeleton

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,7 +1,27 @@
+import Container from "components/Container";
 import React from "react";
 
 const Confirm: React.FC = () => {
-  return <></>;
+  return (
+    <Container>
+      <h1>Confirm You Details</h1>
+      <div className='userCard'>
+        <p>
+          <span>Name:</span>{" "}
+        </p>
+        <p>
+          <span>Phone Number: </span>
+        </p>
+        <p>
+          <span>Address: </span>
+        </p>
+        <p>
+          <span>Email Address:</span>
+        </p>
+      </div>
+      {/* <PokemonCard/> */}
+    </Container>
+  );
 };
 
 export default Confirm;
