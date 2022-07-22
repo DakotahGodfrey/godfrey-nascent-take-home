baedc6db86ccea129ea8a0beaa656862820d5c9a
Dakotah Godfrey <dakotah.godfrey.dev>
2022-07-21 21:48:46 -0400

    connect globalstate to confirmation page

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,25 +1,33 @@
 import Container from "components/Container";
 import React from "react";
+import { useAppDispatch, useAppSelector } from "app/hooks";
+import { clearAll, selectUser } from "app/userSlice";
+import PokemonCard from "components/PokemonCard";
 
 const Confirm: React.FC = () => {
+  const { firstName, lastName, phone, address, email, pokemon } =
+    useAppSelector(selectUser);
+
   return (
     <Container>
       <h1>Confirm You Details</h1>
       <div className='userCard'>
         <p>
-          <span>Name:</span>{" "}
+          <span>Name:</span>
+          {firstName} {lastName}
         </p>
         <p>
-          <span>Phone Number: </span>
+          <span>Phone Number: </span> {phone}
         </p>
         <p>
-          <span>Address: </span>
+          <span>Address: </span> {address}
         </p>
         <p>
-          <span>Email Address:</span>
+          <span>Email Address:</span> {email}
         </p>
       </div>
       {/* <PokemonCard/> */}
+      {pokemon && <PokemonCard pokemon={pokemon} />}
     </Container>
   );
 };
