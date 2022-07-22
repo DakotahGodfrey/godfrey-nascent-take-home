40b459e473afea3b60d2473b32e4e3d743d4f230
Dakotah Godfrey <dakotah.godfrey.dev>
2022-07-21 20:48:23 -0400

    hook in redux-persist library

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import UserReducer from "./userSlice";
-export const store = configureStore({ reducer: { user: UserReducer } });
 
+const persistConfig = {
+  key: "user",
+  version: 1,
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, UserReducer);
+
+export const store = configureStore({
+  reducer: { user: persistedUserReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export let persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
